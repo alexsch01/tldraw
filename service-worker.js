41c61be2cc4499626d495d1cad094385df46a5d9
Alex Schwartz <alexschwartz01@gmail.com>
2024-09-01 10:41:31 -0400

    only caches on install

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const cacheName = 'cache-v1'
 const ROOT_URL = './'
 const precacheResources = ['', 'icon.png', 'index.html', 'main.js', 'manifest.json'].map(elem => ROOT_URL + elem)
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)))
 })
 
